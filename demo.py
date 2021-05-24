cd75cc8cec1bcb098fd606227b89ab41047de2ba
AmrElsersy <amrelsersay@gmail.com>
2021-05-24 17:49:20 +0200

    final_demo
    Signed-off-by: AmrElsersy <amrelsersay@gmail.com>

--- demo.py
+++ demo.py
@@ -61,6 +61,9 @@ def main(args):
         print(f'Time of bisenetv2 = {1000 * (t2-t1)} ms')
         print(f'Time of pointpainting = {1000 * (t3-t2)} ms')
 
+        avg_time += (time_synchronized()-t1)
+        print(f'{i} sample')
+
         if args.mode == '3d':
             screenshot = visualizer.visuallize_pointcloud(painted_pointcloud, blocking=False)
             print(screenshot.shape)
@@ -73,17 +76,18 @@ def main(args):
             elif args.mode == '2d':
                 scene_2D = visualizer.get_scene_2D(color_image, painted_pointcloud, calib)
                 frames.append(scene_2D)
+
+            if args.show:
+
                 cv2.imshow('scene', scene_2D)       
 
-            if cv2.waitKey(0) == 27:
-                cv2.destroyAllWindows()
-                break
+                if cv2.waitKey(0) == 27:
+                    cv2.destroyAllWindows()
+                    break
 
         # if i == 20:
         #     break
 
-        avg_time += (time.time()-t1)
-        print(f'{i} sample')
 
     # Time & FPS
     avg_time /= len(video)
@@ -91,7 +95,7 @@ def main(args):
     print("Average Time",round(avg_time*1000,2), "ms  FPS", round(FPS,2))
     # Save Video
     save_path = os.path.join(args.save_path, 'demo.mp4')
-    out_video = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), 18, frame_shape)
+    out_video = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), FPS, frame_shape)
     for frame in frames:
         frame = cv2.resize(frame, frame_shape)
         out_video.write(frame)
@@ -109,14 +113,15 @@ if __name__ == '__main__':
     parser.add_argument('--video_path', type=str, 
     # default='Videos/2/2011_09_26_drive_0009_sync/2011_09_26/2011_09_26_drive_0009_sync',)
     # default='Videos/1/2011_09_26/2011_09_26_drive_0048_sync',)
-    default='Videos/1/2011_09_26_drive_0093_sync/2011_09_26')
+    default='Videos/2/2011_09_26_drive_0106_sync/2011_09_26')
 
     parser.add_argument('--calib_path', type=str, 
     # default='Videos/2/2011_09_26_calib/2011_09_26',)
-    default='Videos/1/2011_09_26_calib/2011_09_26',)
+    default='Videos/2/2011_09_26_calib/2011_09_26',)
 
     parser.add_argument('--weights_path', type=str, default='BiSeNetv2/checkpoints/BiseNetv2_150.pth',)
     parser.add_argument('--save_path', type=str, default='Videos',)
+    parser.add_argument('--show', action='store_true')
     parser.add_argument('--mode', type=str, default='3d', choices=['img', '2d', '3d'], 
     help='visualization mode .. img is semantic image .. 2d is semantic + bev .. 3d is colored pointcloud')
     args = parser.parse_args()
