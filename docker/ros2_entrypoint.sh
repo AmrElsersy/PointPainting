4ca12635ceae616dc846ea8b85049d0ccbc85a32
markhill343 <markus.hillreiner@hs-augsburg.de>
2023-03-20 13:17:25 +0100

    updated dockerfile and entrypoint

--- docker/ros2_entrypoint.sh
+++ docker/ros2_entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# setup ros environment
-
-if [ ${ROS_DISTRO} == "foxy" ] ; then
-	source "$ROS2_WS/install/local_setup.bash"
-else
-	source "/opt/ros/$ROS_DISTRO/setup.bash" 
-	source "$ROS_WS/devel/setup.bash"
-fi
+# setup ros2 environment
+source "/opt/ros/humble/setup.bash"
 exec "$@"
